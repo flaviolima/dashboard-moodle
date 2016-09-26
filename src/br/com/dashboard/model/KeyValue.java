8502c3b2cf72c2f6a439ec877791e680ed5f58b3
flaviolima <contato.flaviolima@gmail.com>
2016-09-26 09:59:00 -0300

    projeto base

---
+++ src/br/com/dashboard/model/KeyValue.java
@@ -0,0 +1,29 @@
+package br.com.dashboard.model;
+
+public class KeyValue {
+	String key;
+	String value;
+
+	public KeyValue(String key, String value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
